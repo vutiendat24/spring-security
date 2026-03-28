87ff0125ebd318c436ae21a47a6584c16f17936f
vutiendat24 <vutiendattech@gmail.com>
2026-03-28 20:55:07 +0700

    refactor(models): replace String status with UserStatus Enum in User entity

---
+++ src/main/java/com/springboot/spring_security/enums/UserStatus.java
@@ -0,0 +1,8 @@
+package com.springboot.spring_security.enums;
+
+public enum UserStatus {
+    ACTIVE,
+    INACTIVE,
+    BLOCKED,
+    PENDING
+}
--- src/main/java/com/springboot/spring_security/models/User.java
+++ src/main/java/com/springboot/spring_security/models/User.java
@@ -1,10 +1,6 @@
 package com.springboot.spring_security.models;
 
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -12,29 +8,41 @@ import lombok.experimental.FieldDefaults;
 
 import java.util.List;
 import java.util.UUID;
-import jakarta.persistence.ManyToMany;
+import com.springboot.spring_security.enums.UserStatus;
 
 @Entity
 @Getter
 @Setter
 @RequiredArgsConstructor
 @AllArgsConstructor
+@Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     UUID userID;
+
     @Size(min = 5, message="User name must be at least 5 characters")
     String userName;
+
     @Size(min = 8, message="Password must be at least 8 characters")
     String password;
+
     @Size(min=2, message="Full name must be at least 2 characters")
     String fullName;
+
     String phone;
+
     @Email(message="Email is not valid")
     String email;
+
     String sex;
+
     @ManyToMany
     List<Role> roles;
-    boolean isActive = false ;
+
+    boolean isActive = false;
+    
+    @Enumerated(EnumType.STRING)
+    UserStatus status;
 }
